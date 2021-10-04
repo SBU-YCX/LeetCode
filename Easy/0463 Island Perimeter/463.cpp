cd47db703ee15422e723143271da70fd348d8c05
SBU-YCX <56904305+SBU-YCX@users.noreply.github.com>
2021-10-04 13:18:55 -0400

    Update 463.cpp

--- Easy/0463 Island Perimeter/463.cpp
+++ Easy/0463 Island Perimeter/463.cpp
@@ -6,19 +6,19 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int ans = 0;
-        for (int i = 0; i < grid.size(); i++)
-            for (int j = 0; j < grid[0].size(); j++)
+        int m = grid.size(), n = grid[0].size(), ans = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
             {
                 if (grid[i][j]  == 1)
                 {
                     if (i == 0 || i > 0 && grid[i - 1][j] == 0)
                         ans += 1;
-                    if (i == grid.size() - 1 || i < grid.size() - 1 && grid[i + 1][j] == 0)
+                    if (i == m || i < m - 1 && grid[i + 1][j] == 0)
                         ans += 1;
                     if (j == 0 || j > 0 && grid[i][j - 1] == 0)
                         ans += 1;
-                    if (j == grid[0].size() - 1 || j < grid[0].size() - 1 && grid[i][j + 1] == 0)
+                    if (j == n || j < n && grid[i][j + 1] == 0)
                         ans += 1;
                 }
             }
