44da43cfe10711477c05b408bb7f173f1f1a6a43
SBU-YCX <56904305+SBU-YCX@users.noreply.github.com>
2021-10-04 22:58:20 -0400

    Create 238.cpp

---
+++ Medium/0238 Product of Array Except Self/238.cpp
@@ -0,0 +1,26 @@
+//  Topic   ：238. Product of Array Except Self (https://leetcode.com/problems/product-of-array-except-self/)
+//  Author  : YCX
+//  Time    : O(N)
+//  Space   : O(N)
+
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        int prod = 1;
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = prod;
+            prod *= nums[i];
+        }
+        prod = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            ans[i] *= prod;
+            prod *= nums[i];
+        }
+        return ans;
+    }
+};
