36ac33f5729612734be98bfe96e518f3b57cb249
SBU-YCX <56904305+SBU-YCX@users.noreply.github.com>
2021-12-20 20:09:19 -0500

    Create 2099_2.cpp

---
+++ Easy/2099 Find Subsequence of Length K With the Largest Sum/2099_2.cpp
@@ -0,0 +1,22 @@
+//  Topic   : 2099. Find Subsequence of Length K With the Largest Sum (https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/)
+//  Author  : YCX
+//  Time    : O(N + KlogK)
+//  Space   : O(N)
+
+
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        int n = nums.size(), m = n - k;
+        vector<int> idx;
+        for (int i = 0; i < n; i++)
+            idx.push_back(i);
+        nth_element(idx.begin(), idx.begin() + k, idx.end(), [&nums](int a, int b) {return nums[a] > nums[b];});
+        idx.resize(k);
+        sort(idx.begin(), idx.end());
+        vector<int> ans;
+        for (auto& i : idx)
+            ans.push_back(nums[i]);
+        return ans;
+    }
+};
