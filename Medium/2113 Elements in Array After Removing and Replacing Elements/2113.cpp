6e87a7879f5f914801437f1ef9578e195dd7d61c
SBU-YCX <56904305+SBU-YCX@users.noreply.github.com>
2021-12-28 12:51:39 -0500

    Update 2113.cpp

--- Medium/2113 Elements in Array After Removing and Replacing Elements/2113.cpp
+++ Medium/2113 Elements in Array After Removing and Replacing Elements/2113.cpp
@@ -1,6 +1,6 @@
 //  Topic   ：2113. Elements in Array After Removing and Replacing Elements (https://leetcode.com/problems/elements-in-array-after-removing-and-replacing-elements/)
 //  Author  : YCX
-//  Time    : O(M)
+//  Time    : O(N)
 //  Space   : O(1)
 
 
